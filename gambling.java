7ebb6fdf8a28c5c3c6d61113ddb2e37f20f609d8
Antara055 <89896853+Antara055@users.noreply.github.com>
2021-10-20 09:02:24 +0530

    Add files via upload

---
+++ gambling.java
@@ -0,0 +1,13 @@
+class gambling{
+     public void gam(){
+     int everyday_stake = 100;
+     int every_bet = 1;
+     System.out.println("Welcome to gambling");
+     System.out.println("user can start with $" +everyday_stake);
+     System.out.println("bet for every game is $" +every_bet);
+}
+public static void main(String[] args){
+ gambling g = new gambling();
+ g.gam();
+}
+}
